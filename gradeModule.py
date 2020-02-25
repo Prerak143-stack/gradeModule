750e3af540197507420f556c95b0edbe202d05f1
developedbypatels <61062101+developed-bypatels@users.noreply.github.com>
2020-02-25 02:13:57 -0500

    Update gradeModule.py

--- gradeModule.py
+++ gradeModule.py
@@ -1,6 +1,4 @@
-# I am Prerak Patel and my student number is 000736376, I have designed this
-# program by myself and I have not shared this anyone.My professor's name is
-# Tiffany,Antopoloski
+
 
 
 def average( quizWeight, quizAverage, labWeight, labAverage, assignmentWeight, assignmentAverage,\
